f427ff9e90d28b8bb4b752f81f7139a688e74591
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-12 17:15:03 +0000

    views for to apis

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+from tutorials.models import Tutorial
+from tutorials.serializer import TutorialSerializer
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+
+
