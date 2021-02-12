585d972eb5a56600d9564931a7e480492600356b
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-12 17:28:09 +0000

    views for to apis

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,3 +1,4 @@
+import tutorials
 from django.http import request
 from django.shortcuts import render
 from django.http.response import JsonResponse
@@ -14,4 +15,27 @@ from rest_framework.decorators import api_view
 @api_view(['GET','POST','DELETE'])
 def tutorial_list(request):
     if request.method == 'GET':
+        tutorials = Tutorial.objects.all()
+
+        title = request.query_params.get('title', None)
+        if title is not None:
+            tutorials = tutorials.filter(title__icontains=title)
         
+        tutorial_serializer = TutorialSerializer(tutorials, many=True)
+        return JsonResponse(tutorial_serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        tutorial_data = JSONParser().parse(request)
+        tutorial_serializer = TutorialSerializer(data=tutorial_data)
+        if tutorial_serializer.is_valid():
+            tutorial_serializer.save()
+            return JsonResponse(tutorial_serializer.data, status = status.HTTP_201_CREATED)
+        return JsonResponse(tutorial_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 
+
+
+
+
+
+
