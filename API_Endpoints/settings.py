c5fe3ed9313f542f032a226cd5c2424d72e49fa3
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-12 16:47:17 +0000

    startapp tutorials

--- API_Endpoints/settings.py
+++ API_Endpoints/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # DJango RestFramework
+    'rest_framework',
+    #Tutorial 
+    'tutorials.apps.TutorialsConfig',
 ]
 
 MIDDLEWARE = [
