494773e627ae23cd764bafe883217833defaa09a
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-12 17:49:20 +0000

    views for apis

--- tutorials/views.py
+++ tutorials/views.py
@@ -49,7 +49,11 @@ def tutorial_detail(request, pk):
         return JsonResponse(tutorial_serializer.data)
 
     elif request.method == 'PUT':
-        tutorial_data = JSONParser().parse(request)  
+        tutorial_data = JSONParser().parse(request)
+        tutorial_serializer = TutorialSerializer(tutorial, data=tutorial_data)
+        if tutorial_serializer.is_valid():
+            tutorial_serializer.save()
+            return JsonResponse()
 
 
 
