5edc4a622c13452348e2d1d9bf75b672da37e3c6
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-12 17:00:58 +0000

    added serializer to tutorial

---
+++ tutorials/serializer.py
@@ -0,0 +1,13 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Tutorial
+
+class TutorialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tutorial
+        fields = (
+            'id',
+            'title',
+            'description'
+        )
+
