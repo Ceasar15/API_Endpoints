9697916ebe656176fb4229c0827afd5eab81882d
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-13 18:25:55 +0000

    implementing models of tickets

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+def generate_ticket_id():
+    return str(uuid.uuid()).split("-")[-1]
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    ticket_id = models.CharField(max_length=255, blank=True)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
