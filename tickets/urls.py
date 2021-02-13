3ada245692c4ccf74c7b0b2e578d7537d4781960
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-13 19:09:14 +0000

    urls for serializers

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path, include 
 
 from django.contrib.auth.models import User
-from tickets.models
+from rest_framework import routers
+
+from tickets.models import Ticket, Category
+from tickets.views import UserViewSet, TicketViewSet, CategoryViewSet
+
