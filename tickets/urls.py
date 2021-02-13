fcec18d8beba91deb8060d346f8bdd8e8c30f104
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-13 19:16:25 +0000

    urls for serializers

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,7 +2,21 @@ from django.urls import path, include
 
 from django.contrib.auth.models import User
 from rest_framework import routers
+from rest_framework import urlpatterns
 
 from tickets.models import Ticket, Category
 from tickets.views import UserViewSet, TicketViewSet, CategoryViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'api/users', UserViewSet)
+
+router.register(r'api/tickets', TicketViewSet)
+
+router.register(r'api/category', CategoryViewSet)
+
+
+urlpatterns = [
+    path(r'', include(router.urls)),
+]
 
