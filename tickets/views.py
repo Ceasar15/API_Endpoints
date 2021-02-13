6a27eb06df862e3ea47f01ce424b70e43bb89c56
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-13 19:00:30 +0000

    serializers models

--- tickets/views.py
+++ tickets/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from rest_framework import routers, serializers, viewsets
 from tickets.serializers import UserSerializer, TicketSerializer, CategorySerializer
 from django.contrib.auth.models import User
-from tickets.models 
+from tickets.models import 
 
 # Create your views here.
