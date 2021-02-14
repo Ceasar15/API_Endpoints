2e99235cc8e4dfb57d6e3ef4a1db53792514b66c
Ceasar15 <ceasarkwadwo@gmail.com>
2021-02-14 23:03:06 +0000

    JSONDecode Error

--- geo_location/views.py
+++ geo_location/views.py
@@ -1,11 +1,12 @@
 from django.http import response
 from django.shortcuts import render
 import requests
+import json
 
 # Create your views here.
 
 def home(request):
-    response = requests.get('http://freegeoip.net/json')
+    response = requests.get('http://freegeoip.net/json').json()
     geodata = response.json()
     context = {
         'ip': geodata['ip'],
