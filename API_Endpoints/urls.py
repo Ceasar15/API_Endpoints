33be0d6d2b70d09eb7db8a980385fe25bdcc805e
Ceasar <ceasarkwadwo@gmail.com>
2021-10-22 10:17:07 +0000

    included docs for documentation'

--- API_Endpoints/urls.py
+++ API_Endpoints/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
 
 schema_view = get_schema_view(title='API ENDPOINTS')
 
@@ -30,4 +32,5 @@ urlpatterns = [
     path('api/auth/', include('rest_auth.urls')),
     path('api/auth/reg/', include('rest_auth.registration.urls')),
     path('schema/', schema_view),
+    path('docs/', include_docs_urls(title='API_ENDPOINTS'))
 ]
